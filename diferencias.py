361a15c9ce586d2b57c5d608d85e9d23907cae8e
carletti <trcarletti@gmail.com>
2016-06-08 03:30:31 -0300

    melhorias

--- diferencias.py
+++ diferencias.py
@@ -27,50 +27,54 @@ def derivadas(I, t):
     
     # Funções 
     v = ((vx**2)+(vy**2))**(1/2) # Velocidade 
+    print(v)
     Ce = 2*math.pi*Cd0 # Coeficiente de empuxo 
     Cdi = (Ce**2)/(math.pi*((S**2)/A)*e) # Coenficiente de ???? 
-    Ca = Cd0*Cdi # Coeficiente de atrito 
+    Ca = Cd0+Cdi # Coeficiente de atrito 
     E = (Ce*ro*(v**2)*A)/2 # Empuxo 
     Fres = (Ca*ro*(v**2)*A)/2 # Força de resistência do ar  
     P = m*g # Força peso 
-    I = ((d**3)*m)/3 # Momento de inércia
-    Tr = I*o #torque
-
-    Ex = E*math.cos(a) 
-    Ey = E*math.sin(a)
-    Fresx = Fres*math.cos(a) 
-    Fresy = Fres*math.sin(a)
+    i = (d*m)/3 # Momento de inércia
+    Tr = E*d #torque
+    Ex = E*math.cos(a) # Empuxo em x
+    Ey = E*math.sin(a) # Empuxo em y
+    Fresx = Fres*math.cos(a) # Força de resistência do ar em x
+    Fresy = Fres*math.sin(a) # Foça de resistênia do as em y
     
     # Diferenciais 
-    dadt = o
-    dodt = I/Tr
     dxdt = vx 
     dydt = vy 
-    dvxdt = (Ex - Fresx)/m 
+    dvxdt = (-Ex - Fresx)/m 
     dvydt = (Ey + Fresy - P)/m 
+    dadt = o
+    dodt = i/Tr
     return [dxdt, dydt, dvxdt, dvydt, dadt, dodt] 
-
 
 # Condições iniciais     
 x0 = 0 #cm
 y0 = 0.7 #cm 
 vx0 = 3.16 #m/s
 vy0 = 3.16 #m/s
-a0 = 0 # Ângulo de lançamento
+a0 = np.pi/4 # Ângulo de lançamento
 o0 = 0 # Velocidade angular 
 I0 = [x0, y0, vx0, vy0, a0, o0] 
-
  
 # Lista tempo 
-t = np.arange(0,10,0.01) 
-
+t = np.arange(0,5,0.01) 
  
 # Chamada da odeint 
 r = odeint(derivadas, I0, t) 
+print(r[:,0])
+#print(r[:,1])
+#print(r[:,2])
+#print(r[:,3])
+#print(r[:,4])
+#print(r[:,5])
 
- 
 # Plotagem dos gráficos 
+
 # Gráfico posição X 
+
 plt.plot(t, r[:, 0]) 
 #plt.ScalarFormatter(0, 0.00000001) 
 plt.legend(loc = 'upper right') 
@@ -79,6 +83,7 @@ plt.xlabel('Tempo (segundos)')
 plt.title(r'Posição em X') 
 plt.grid(True) 
 plt.show()
+
 # Gráfico posição Y 
 plt.plot(t, r[:, 1]) 
 #plt.ScalarFormatter(0, 0.00000001) 
@@ -88,6 +93,7 @@ plt.xlabel('Tempo (segundos)')
 plt.title(r'Posição em Y') 
 plt.grid(True) 
 plt.show()
+
 # Gráfico Velocidade X 
 plt.plot(t, r[:, 2]) 
 #plt.ScalarFormatter(0, 0.00000001) 
@@ -97,6 +103,7 @@ plt.xlabel('Tempo (segundos)')
 plt.title(r'Velocidade X') 
 plt.grid(True) 
 plt.show()
+
 # Gráfico Velocidade y 
 plt.plot(t, r[:, 3]) 
 #plt.ScalarFormatter(0, 0.00000001) 
@@ -106,3 +113,12 @@ plt.xlabel('Tempo (segundos)')
 plt.title(r'Velocidade Y') 
 plt.grid(True) 
 plt.show()
+
+# Testes
+plt.plot(t, r[:4])
+plt.show()
+plt.plot(t, r[:5])
+plt.show()
+plt.plot(r[:,0], r[:1])
+plt.show()
+
